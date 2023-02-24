351b4fa805957e178e35d59d84e81652a7457bc1
nshutinicolas <nshuti.nicolas@yahoo.com>
2023-02-24 11:05:31 +0200

    Initial Commit

---
+++ DailyTaskManager/DailyTaskManagerApp.swift
@@ -0,0 +1,19 @@
+//
+// DailyTaskManagerApp.swift
+// DailyTaskManager
+//
+// Created by Nicolas Nshuti on 24/02/2023
+// Copyright © 2023 GHOST TECHNOLOGIES LLC. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct DailyTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
